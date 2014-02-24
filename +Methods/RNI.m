f321c5167ce853ae537febbc337f1e0493953aea
Andreas Tjärnberg <andreas.tjarnberg@scilifelab.se>
2014-02-24 17:28:16 +0100

    Adding alpha input variable for different confidense levels in RNI

--- +Methods/RNI.m
+++ +Methods/RNI.m
@@ -5,6 +5,7 @@ function varargout = RNI(varargin)
 %   ================
 %   data:    GeneSpider.Dataset
 %   net:     GeneSpider.Network <optional>
+%   alpha:   confidense level of inference.
 %
 %   Output Arguments: confA
 %   =================
@@ -25,7 +26,7 @@ for i=1:nargin
     elseif isa(varargin{i},'logical')
         rawZeta = varargin{i}; % Obsolete
     else
-        zetavec = varargin{i}; % Obsolete
+        alpha = varargin{i}; % Obsolete
     end
 end
 
@@ -33,42 +34,36 @@ if ~exist('data')
     error('needs a data set')
 end
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%% Determine how to handle zeta %%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-% if ~rawZeta
-%     zetaRange = [];
-%     estA = -data.P*pinv(response(data,net));
-%     zetaRange(1) = min(abs(estA(estA~=0)))-eps;
-%     zetaRange(2) = max(abs(estA(estA~=0)))+10*eps;
-
-%     % Convert to interval.
-%     delta = zetaRange(2)-zetaRange(1);
-%     zetavec = zetavec*delta + zetaRange(1);
-% end
+if ~exist('alpha')
+    alpha = 0.01;
+end
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-lambda = data.lambda;
-if numel(lambda) == 2
-    o = ones(size(data.P));
-    [Gamma, PS, NrPS] = tools.RInorm(response(data,net)',data.P',diag(lambda(1:length(lambda)/2))*o',diag(lambda(length(lambda)/2+1:end))*o'+eps,data.alpha);
-else
-    o = ones(size(data.P),2);
-    [Gamma, PS, NrPS] = tools.RInorm(response(data,net)',data.P',(diag(lambda(1:length(lambda)/2))'*o)',(diag(lambda(length(lambda)/2+1:end))'*o)'+eps,data.alpha);
+for i=1:length(alpha)
+    lambda = data.lambda;
+    if numel(lambda) == 2
+        o = ones(size(data.P));
+        [gamma, ps, Nrps] = tools.RInorm(response(data,net)',data.P',diag(lambda(1:length(lambda)/2))*o',diag(lambda(length(lambda)/2+1:end))*o'+eps,alpha(i));
+    else
+        o = ones(size(data.P),2);
+        [gamma, ps, Nrps] = tools.RInorm(response(data,net)',data.P',(diag(lambda(1:length(lambda)/2))'*o)',(diag(lambda(length(lambda)/2+1:end))'*o)'+eps,alpha(i));
+    end
+    Gamma(:,:,i) = gamma;
+    PS(:,:,i) = double(ps);
+    NrPS(i) = Nrps;
 end
 
 if nargout == 0,
     disp(Gamma);
 elseif nargout == 1;
-    varargout{1} = double(PS);
+    varargout{1} = PS;
 elseif nargout == 2;
-    varargout{1} = double(PS);
+    varargout{1} = PS;
     varargout{2} = Gamma;
 elseif nargout == 3;
-    varargout{1} = double(PS);
+    varargout{1} = PS;
     varargout{2} = Gamma;
     varargout{3} = NrPS;
 end
