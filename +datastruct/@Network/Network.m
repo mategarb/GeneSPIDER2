c9ef3f4b7d0b1793bba0df4716dc5913e6065a0d
Andreas Tjärnberg <andreas.tjarnberg@scilifelab.se>
2014-02-27 16:25:17 +0100

    Show network should work better, this is a start. (It's actually worse now)

--- +datastruct/@Network/Network.m
+++ +datastruct/@Network/Network.m
@@ -111,10 +111,11 @@ classdef Network < hgsetget
                 '# Nodes'              size(net.A,1)
                 '# links'              nnz(net)};
 
-            f = figure('Position', [100 100 752 500]);
+            f = figure();
             t1 = uitable('Parent', f, 'Position', [10 10 730 310],'ColumnWidth',{65});
-            set(t1, 'Data', net.A,'BackgroundColor',[1,1,1]);
-            t2 = uitable('Parent', f, 'Position', [10 330 730 170],'ColumnWidth',{200,480});
+            set(t1, 'Data', net.A,'ColumnName',net.names,'RowName',net.names,'BackgroundColor',[1,1,1]);
+            f = figure();
+            t2 = uitable('Parent', f,'ColumnWidth',{200,480})
             set(t2, 'Data', networkProperties,'BackgroundColor',[1,1,1],'ColumnName',{'Property','Value'});
         end
 
