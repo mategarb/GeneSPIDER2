f51958b4d59abd65a622f70264cd83732cdfd0dc
Andreas Tjärnberg <andreas.tjarnberg@scilifelab.se>
2014-11-07 12:35:53 +0100

    Correction to variable order and names

--- +gsUtilities/tls.m
+++ +gsUtilities/tls.m
@@ -1,4 +1,4 @@
-function [hatTheta hatXi hatPhi] = tls(Xi,Phi)
+function [hatTheta hatXi hatPhi] = tls(Phi,Xi)
 % hatTheta = tls(Xi,Phi)
 % Basic total least squares estimation algorithm (cite:Markovsky2007)
 % 
@@ -9,15 +9,13 @@ function [hatTheta hatXi hatPhi] = tls(Xi,Phi)
 % $\Theta = A^T$
 %
 
-m        = size(Phi,1);        % number of x,y data pairs
-X        = Xi;
-Y        = Phi;
-n        = size(Xi,2);         % n is the width of X (X is m by n)
-Z        = [Xi Phi];           % Z is X augmented with Y.
+m        = size(Xi,1);         % number of x,y data pairs
+n        = size(Phi,2);        % n is the width of X (X is m by n)
+Z        = [Phi Xi];           % Z is X augmented with Y.
 [U S V]  = svd(Z,0);           % find the SVD of Z.
 VXY      = V(1:n,1+n:end);     % Take the block of V consisting of the first n rows and the n+1 to last column
 VYY      = V(1+n:end,1+n:end); % Take the bottom-right block of V.
-hatTheta = -VXY/VYY;
+hatTheta = -VXY/VYY;           % Generate TLS estimate
 
 S1 = S;
 S2 = S;
