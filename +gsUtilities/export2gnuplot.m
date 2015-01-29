c434089bab529087ba782658d47025d653b37d93
Andreas Tjärnberg <andreas.tjarnberg@scilifelab.se>
2015-01-29 17:33:38 +0100

    Bugfixing and extending documentation for export2gnuplot.m

--- +gsUtilities/export2gnuplot.m
+++ +gsUtilities/export2gnuplot.m
@@ -1,17 +1,16 @@
 function export2gnuplot(file,varargin)
-%   function that exports vectors and variables to gnuplot tsv data file
-%   format.
+%   function for exporting vectors and variables to a gnuplot friendly tsv format.
+%   export2gnuplot(file{,variables,data})
 %
-%   export2gnuplot(file,variables,data)
 %
 %   Input arguments:
 %   ===============
-%   file: path and filenname to store data in. If file
+%   file: path and filename to store data in. If file
 %         exist the file will be appended with the new data
 %
 %   variable input arguments:
 %   ========================
-%   variables: variable names if any. Cell array with strings
+%   variables: variable names if any else put in empty cell array {}. Cell array with strings
 %   data: 2d matrix, where each row represent a data point.
 %
 %   File structure:
@@ -22,17 +21,20 @@ function export2gnuplot(file,varargin)
 %   data(1,1)     data(1,2)   ... data(1,n-1)   data(1,n)
 %       .             .       ...     .             .
 %       .             .       ...     .             .
+%   data(n-1,1)   data(n-1,2) ... data(n-1,n-1) data(n-1,n)
 %   data(n,1)     data(n,2)   ... data(n,n-1)   data(n,n)
 %
 %
 %   # Next block
 %
+%   ==============
 %   A file Header is defined by having an empty data source for the first variable data pair.
+%
 
     nArgs = length(varargin);
-    if round(nArgs/2) ~= nArgs/2
-        disp(optionNames)
-        error([parentFunc,' needs propertyName/propertyValue pairs with names as above'])
+    if ~logical(mod(nArgs,2)) && nArgs == 0
+        tmp = dbstack;
+        error([tmp.name,' needs variables (cell array)/data (2d matrix) pairs as input, see: help tools.export2gnuplot'])
     end
 
     [path,f,ext] = fileparts(file);
@@ -53,7 +55,7 @@ function export2gnuplot(file,varargin)
 
     for i=1:2:nArgs
         variables = varargin{i};
-        values = varargin{i=1};
+        values = varargin{i+1};
 
         fprintf(fid,'#');
         fprintf(fid,'\t%s',variables{:});
