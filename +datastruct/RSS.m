08a4798b919de16712c7a20325fef2e3d25e2134
Andreas Tjärnberg <andreas.tjarnberg@scilifelab.se>
2015-05-22 11:24:43 +0200

    Set covariance of P to 1 if it doesn't exist

--- +datastruct/RSS.m
+++ +datastruct/RSS.m
@@ -1,10 +1,12 @@
-function [RSSy RSSp] = RSS(Alist,data,i,type)
+function varargout = RSS(Alist,data,i,type)
 % Function for calculating prediction Error PE for y and p,
 %
 % [RSSy RSSp] = RSS(Alist,data,i,type)
+%
 % If type='WRSS' is given then the weighted residual sum of squares is
-% caluculated.
+% calculated.
 % Otherwise the residual sum of squares is calculated.
+%
 
 if nargin < 4,
     type = 'RSS';
@@ -27,10 +29,17 @@ for j = 1:size(Alist,3)
         [cvY, cvP] = cov(data);
         if ~isempty(data.cvY), cvY = data.cvY; end
         if ~isempty(data.cvP), cvP = data.cvP; end
+        if nnz(cvP) == 0, cvP = 1; end
         RSSy(j) = (Y(:,i) - y)'*pinv(cvY)*(Y(:,i) - y); % WRSS
         RSSp(j) = (P(:,i) - p)'*pinv(cvP)*(P(:,i) - p); % WRSS
     else
         RSSy(j) = norm(Y(:,i) - y, 'fro')^2;
         RSSp(j) = norm(P(:,i) - p, 'fro')^2;
     end
+end
+
+varargout{1} = RSSy;
+
+if nargout > 1
+    varargout{2} = RSSp;
 end
