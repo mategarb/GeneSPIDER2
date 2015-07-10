22f69ae4f65a4c8cfd56376d2f6fe85ad9b59836
Andreas Tjärnberg <andreas.tjarnberg@scilifelab.se>
2015-07-10 18:51:41 +0200

    Fix sign reverse bug

--- +gsUtilities/tls.m
+++ +gsUtilities/tls.m
@@ -15,7 +15,7 @@ Z        = [Phi Xi];           % Z is X augmented with Y.
 [U S V]  = svd(Z,0);           % find the SVD of Z.
 VXY      = V(1:n,1+n:end);     % Take the block of V consisting of the first n rows and the n+1 to last column
 VYY      = V(1+n:end,1+n:end); % Take the bottom-right block of V.
-hatTheta = -VXY/VYY;           % Generate TLS estimate
+hatTheta = VXY/VYY;            % Generate TLS estimate
 
 S1 = S;
 S1(n+1:end,n+1:end) = 0;
