a3322f119cd759d7c06e5f65b81be39da90f4e81
Andreas Tjärnberg <andreas.tjarnberg@scilifelab.se>
2015-07-13 14:18:28 +0200

    Fixing function and documentation

--- +datastruct/cutSym.m
+++ +datastruct/cutSym.m
@@ -1,18 +1,19 @@
-function A = cutSym(A,inchance)
+function A = cutSym(A,inchance,feedback_fraction)
 % removes links in a symmetric matrix with a probability to be in or out degree.
 % A small chance of keeping feedback loops.
 %
-% A = datastruct.cutSym(A,inchance)
+% A = datastruct.cutSym(A,inchance,feedback_fraction)
 %
-% A: network matrix nxm n=outdegree, m=indegree
-% inchance: the chance of removing in n instead of m
+% A                 : network matrix nxn where i=outgoing, j=incoming
+% inchance          : the chance of removing in i instead of j
+% feedback_fraction : feedback = inchance/X where feedback_fraction=X. (default = 20)
 
 N = size(A,1);
 
-if inchance < 0.5
+if ~exist(feedback_fraction,'var')
     feedback = inchance/20;
 else
-    feedback = (1-inchance)/20;
+    feedback = inchance/feedback_fraction;
 end
 
 inchance = inchance - feedback;
