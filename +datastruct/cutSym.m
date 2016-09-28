f579cf70a094dad45034f5c32aaed249a2f17b82
Andreas Tjärnberg <andreas.tjarnberg@fripost.org>
2016-09-28 10:35:24 +0200

    Simplifying cutSym to make it easier to understand and use

--- +datastruct/cutSym.m
+++ +datastruct/cutSym.m
@@ -1,30 +1,25 @@
-function A = cutSym(A,inchance,feedback_fraction)
+function A = cutSym(A,pin,pout)
 % removes links in a symmetric matrix with a probability to be in or out degree.
 % A small chance of keeping feedback loops.
 %
-% A = datastruct.cutSym(A,inchance,feedback_fraction)
+% A = datastruct.cutSym(A,pin,pout)
 %
 % A                 : network matrix nxn where i=outgoing, j=incoming
-% inchance          : the chance of removing in i instead of j
-% feedback_fraction : feedback = inchance/X where feedback_fraction=X. (default = 20)
+% pin               : the chance of removing in i instead of j
+% pout              : the chance of removing in j instead of i
+%
+% the probability of a feedback loop is then
+% pfeedback         : 1 - (pin+pout)
 
 N = size(A,1);
-
-if ~exist('feedback_fraction','var')
-    feedback = inchance/20;
-else
-    feedback = inchance/feedback_fraction;
-end
-
-inchance = inchance - feedback;
 
 for i=1:N-1
     for j=i+1:N
         if A(i,j) == 1
             r = rand;
-            if r < inchance
+            if r <= pin
                 A(i,j) = 0;
-            elseif r > inchance+feedback
+            elseif r <= pin+pout
                 A(j,i) = 0;
             end
         end
